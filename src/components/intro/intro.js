6d6852feb1ae04160be1d68fac5a1f7f6a56c41a
Kapil Bodas <bodas.rushabhdev.14it5005@gmail.com>
2024-03-05 23:04:58 +0530

    Changes done in Intro by Kapil

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () =>{
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Kapil S. Bodas</span><br/>Undergrad Student of VESIT Chembur</span>
-                <span className="introText">pursuing BE in Artificial Intelligence & Data Science</span>
+                <span className="introText">pursuing BE in Artificial Intelligence & Data Science </span>
                 <br/>
                 <p className="introPara">Enthusiasts in Cloud Computing and Java. </p>
                 
